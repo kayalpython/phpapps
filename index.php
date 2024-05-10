7207a89e22dd9b0ccb274e3c38ff00713af2552a
kayalpython <126571384+kayalpython@users.noreply.github.com>
2024-05-10 17:50:47 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Project</title>
+</head>
+<body>
+    <h1>Welcome to PHP Project</h1>
+    <?php
+    echo "Hello, World!";
+    ?>
+</body>
+</html>
